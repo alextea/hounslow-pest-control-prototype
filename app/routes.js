b61c0d05794af6c4501a649c1a3cd4f7910ed253
Alex Torrance <hello@alextorrance.co.uk>
2021-02-16 23:53:13 +0000

    Initial page flow

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,60 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/type-of-pest', function (req, res) {
+  let pestTypes = [
+    {
+      value: 'rats',
+      text: 'Rats'
+    },
+    {
+      value: 'mice',
+      text: 'Mice'
+    },
+    {
+      value: 'squirrels',
+      text: 'Squirrels'
+    },
+    {
+      value: 'fleas',
+      text: 'Fleas'
+    },
+    {
+      value: 'cockroaches',
+      text: 'Cockroaches'
+    },
+    {
+      value: 'bedbugs',
+      text: 'Bedbugs'
+    },
+    {
+      value: 'pharaoh-ants',
+      text: 'Pharaoh ants'
+    },
+    {
+      value: 'garden-ants',
+      text: 'Garden ants'
+    },
+    {
+      value: 'wasps',
+      text: 'Wasps'
+    },
+    {
+      value: 'pigeons',
+      text: 'Pigeons nesting in loft, etc'
+    },
+    {
+      divider: 'or'
+    },
+    {
+      value: 'unknown',
+      text: 'I’m not sure'
+    }
+  ]
+
+  res.render('type-of-pest', {
+    'pestTypes': pestTypes
+  })
+})
+
 module.exports = router
